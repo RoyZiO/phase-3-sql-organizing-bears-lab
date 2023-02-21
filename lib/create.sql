34c4321222295fc0ebb7485d84d3dd1d35e582c3
RoyZiO <nziokinroy@gmail.com>
2023-02-21 10:14:27 +0300

    Completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER VARCHAR(25), sex TEXT, color TEXT, temperament TEXT, alive BOOLEAN)
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr.Choclate', 18, 'M', 'Brown', 'Calm', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 14, 'M', 'Black', 'Rowdy', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 19, 'F', 'Burgundy', 'Carefree', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergent Brown', 10, 'M', 'Black', 'Neutral', 'No');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 27, 'F', 'Grey', 'Sociable', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 24, 'M', 'Green', 'Persistent', 'No');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 18, 'F', 'Light Brown', 'Active', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(null, 17, 'M', 'black', 'angry', 0);
